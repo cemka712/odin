c2e59856a9a29dae701e5d8d05dc762188a62370
cemka712 <pogorelovsemyon@gmail.com>
2026-08-13 14:04:23 +0700

    feat: hello world

--- beg.py
+++ beg.py
@@ -1,11 +1,16 @@
-#!/usr/bin/env python3
+
+
 import pygame 
+
+
 def hello_world(x):
     if x == 'print':
         print('Hello World!')
     else:
         print(x)
+
 hello_world('print')
 pygame.init()
 screen = pygame.display.set_mode()
 
+print('hello world')
